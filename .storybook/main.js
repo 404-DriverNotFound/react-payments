8d478fd51d58fb672ea14c74aef0ac70c673d318
kmin <rudgks114@gmail.com>
2021-07-06 02:10:26 +0900

    fix: storybook path resolve 에러 해결
    path를 alias로 축약해서 "@components"등으로 사용하는데 storybook에서 이 alias를 정상적으로 해결하지 못함.
    
    따라서 .storybook/main.js를 수정하였음.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias['@components'] = path.resolve(__dirname, '../src/components');
+    config.resolve.alias['@constant'] = path.resolve(__dirname, '../src/constant');
+    return config;
+  },
+};
