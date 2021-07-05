8251705da95b406fcfa9e998a237ed67fc25fcef
Kyunghan Min <kmin@c10r8s4.42seoul.kr>
2021-07-05 16:59:25 +0900

    fix: add Component position fix

--- src/components/Add/add.tsx
+++ src/components/Add/add.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import FlexCenter from '@components/FlexCenter/flexCenter'
 import './add.css';
+import View from '@constant/constant';
 
-const Add = () => {
+interface AddProps {
+  onClick: (value: number) => void;
+}
+
+const Add = ({ onClick }: AddProps) => {
+  const goToAddView = () => {
+    onClick(View.Add);
+  };
   return (
     <>
-      <div className={`card`}>
+      <div className={`card`} onClick={goToAddView}>
         <FlexCenter>
           <div className={`add`}>
             <div className={`plus`}>
