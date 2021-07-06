196a0ffc818871f711c21cc4f2a03b229f19f9c4
kmin <rudgks114@gmail.com>
2021-07-07 02:19:53 +0900

    feat: publisher 컴포넌트 props 전달

--- src/components/CardPublishForm/cardPublishForm.tsx
+++ src/components/CardPublishForm/cardPublishForm.tsx
@@ -3,10 +3,17 @@ import Label from '@components/share/Label/label';
 import Input from '@components/share/Input/input';
 
 interface CardPublisherFormProps {
+  publisher: string;
+  setPublisher: (value: string) => void;
   publisherInput: React.RefObject<HTMLInputElement>;
 }
 
-const CardPublishForm = ({ publisherInput }: CardPublisherFormProps) => {
+const CardPublishForm = ({ publisher, setPublisher, publisherInput }: CardPublisherFormProps) => {
+  const handlePublisherChange = ({ target }: { target: HTMLInputElement }) => {
+    // TODO publisher 검증
+    setPublisher(target?.value);
+  };
+  
   return (
     <>
       <Label value={'카드 소유자 이름'}>
@@ -14,7 +21,10 @@ const CardPublishForm = ({ publisherInput }: CardPublisherFormProps) => {
           ref={publisherInput}
           maxLength={'30'}
           size={'30'}
-          placeholder={'카드에 표시된 이름과 동일하게 입력하세요'}/>
+          placeholder={'카드에 표시된 이름과 동일하게 입력하세요'}
+          value={publisher}
+          onChange={handlePublisherChange}
+        />
       </Label>
     </>
   );
